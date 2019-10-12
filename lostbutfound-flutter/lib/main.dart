16fc9474bb07ac1e0b2bb8676910ca4905127394
vigneshwar221B <vigneshwar221b@gmail.com>
2019-10-12 13:43:29 +0530

    added flutter

---
+++ lostbutfound-flutter/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import './pages/loginpage.dart';
+import './pages/mainPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
+var host='http://87046cfe.ngrok.io/';
+var change = '';
+var port='80';
+var path='87046cfe.ngrok.io';
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+	debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState(){
+    print("main dart initstate");
+    super.initState();
+    SharedPreferences.getInstance().then((prefs){
+      setState(() {
+        print(change);
+        if(prefs.getString('token') == null){
+          change = '';
+        }
+        else{
+          change = prefs.getString('token');
+        }
+        
+        print(change);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return change == '' ? LoginPage() : MainPage();
+  }
+}
