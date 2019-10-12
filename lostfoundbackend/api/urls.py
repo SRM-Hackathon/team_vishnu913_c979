fe43bdb247b9280be65600bb09f3b60031daaea8
vigneshwar <40354785+vigneshwar221B@users.noreply.github.com>
2019-10-12 12:45:31 +0530

    Update urls.py

--- lostfoundbackend/api/urls.py
+++ lostfoundbackend/api/urls.py
@@ -6,9 +6,12 @@ urlpatterns = [
     path('found/',views.FoundPostView.as_view()),
     path('lost/',views.LostPostView.as_view()),
     path('cam/',views.CamView.as_view()),
-    path('myfound/',views.MyFoundView.as_view()),
-    path('mylost/',views.MyLostView.as_view()),
+    path('foundhome/',views.FoundHomeView.as_view()),
+    path('losthome/',views.LostHomeView.as_view()),
     path('dummy/',views.Dummy.as_view()),
+    path('genfound/',views.FoundView.as_view()),
+    path('genlost/',views.LostView.as_view())     ,
+    path('bot/',views.BotView.as_view())                                                                                                                                                  
     #path('lostrecord/',views.LostRecordView.as_view())
     
 ]
